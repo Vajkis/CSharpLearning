57024c2913de849a88538865969fda7844b3d8bb
Vajkis <vajkis1@gmail.com>
2025-02-27 10:50:44 +0200

    test

--- obj/Debug/net8.0/CSharpLearning.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharpLearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpLearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1c01e48465c82ddfbd4a1a4ae89354fbb501bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0422f6e4cfd2ee998880747078afc2c2a909e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpLearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpLearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
